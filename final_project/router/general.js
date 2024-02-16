c34ea075e978995a13ee98fd40c498d4e16e4681
mikebashford <mikebashford@gmail.com>
2024-02-16 11:09:30 -0600

    Added ability to register to site and look at reviews based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,7 +6,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (username !== "") {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
@@ -91,10 +103,11 @@ public_users.get("/review/:isbn", function (req, res) {
     );
   });
   let book = allBooks.filter((book) => book !== null)[0];
-  if (book) {
-    return res.send(book);
+  console.log(book.reviews);
+  if (book.reviews === "") {
+    return res.send(book.reviews);
   } else {
-    return res.status(300).json({ message: "No books found by this author" });
+    return res.status(300).json("No reviews available");
   }
 });
 
